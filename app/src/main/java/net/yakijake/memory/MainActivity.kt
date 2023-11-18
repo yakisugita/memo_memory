224f3a2125e5106bd5eeb12419aabcc0ed206a04
yakijake <yakisugita@gmail.com>
2023-11-18 15:33:47 +0900

    写真ズーム処理

--- app/src/main/java/net/yakijake/memory/MainActivity.kt
+++ app/src/main/java/net/yakijake/memory/MainActivity.kt
@@ -6,14 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
@@ -24,21 +19,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import net.yakijake.memory.ui.theme.MemoryTheme
 
@@ -70,15 +58,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 @Composable
 fun Test(modifier: Modifier = Modifier) {
-    var sliderPosition by remember { mutableStateOf(0.5f) }
-    var sliderPosition2 by remember { mutableStateOf(0.5f) }
+    var sliderPosition by remember { mutableStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
-    var scale by remember { mutableStateOf(1f) }
+    var scale by remember { mutableStateOf(2f) }
     var CustomShape by remember { mutableStateOf(GenericShape { size, layoutDirection -> }) }
-    Text(
-        text = sliderPosition.toString(),
-        modifier = Modifier.padding(all = 8.dp)
-    )
     Image(
         painter = painterResource(id = R.drawable.original),
         contentDescription = "An Image",
@@ -86,19 +69,21 @@ fun Test(modifier: Modifier = Modifier) {
         modifier = Modifier
             .pointerInput(Unit) {
                 detectTransformGestures { _, pan, zoom, _ ->
-//                    scale *= zoom
+                    scale *= zoom
+                    if (scale < 1) scale = 1f
+                    if (scale > 10) scale = 10f
                     offset += pan
                     CustomShape = GenericShape { size, layoutDirection ->
                         moveTo(0f, 0f)
-                        lineTo((size.width*sliderPosition)+offset.x, 0f)
-                        lineTo((size.width*sliderPosition)+offset.x, size.height)
+                        lineTo((size.width*sliderPosition)+offset.x+(sliderPosition-0.5f)*scale*920, 0f)
+                        lineTo((size.width*sliderPosition)+offset.x+(sliderPosition-0.5f)*scale*920, size.height)
                         lineTo(0f, size.height)
                     }
                 }
             }
             .graphicsLayer {
-                scaleX = 2f
-                scaleY = 2f
+                scaleX = scale
+                scaleY = scale
                 translationX = offset.x
 //                translationY = offset.y
             }
@@ -114,29 +99,45 @@ fun Test(modifier: Modifier = Modifier) {
             .fillMaxHeight()
             .clip(CustomShape) // 作成したカスタムShapeで切り抜く
             .graphicsLayer {
-                scaleX = 2f
-                scaleY = 2f
+                scaleX = scale
+                scaleY = scale
                 translationX = offset.x
 //                translationY = offset.y
             }
     )
+
     Column {
+        Text(
+            text = sliderPosition.toString(),
+            modifier = Modifier.padding(all = 8.dp)
+        )
+        Text(
+            text = (offset.x).toString(),
+            modifier = Modifier.padding(all = 8.dp)
+        )
+        Text(
+            text = (scale).toString(),
+            modifier = Modifier.padding(all = 8.dp)
+        )
+        Text(
+            text = (offset.x/scale).toString(),
+            modifier = Modifier.padding(all = 8.dp)
+        )
+        Text(
+            text = ((sliderPosition-0.5f)*scale).toString(),
+            modifier = Modifier.padding(all = 8.dp)
+        )
         Slider(
             value = sliderPosition,
             onValueChange = {
                 sliderPosition = it
                 CustomShape = GenericShape { size, layoutDirection ->
                     moveTo(0f, 0f)
-                    lineTo((size.width*sliderPosition)+offset.x, 0f)
-                    lineTo((size.width*sliderPosition)+offset.x, size.height)
+//                    lineTo((size.width*sliderPosition)+(offset.x/scale), 0f)
+                    lineTo((size.width*sliderPosition)+offset.x+(sliderPosition-0.5f)*scale*920, 0f)
+                    lineTo((size.width*sliderPosition)+offset.x+(sliderPosition-0.5f)*scale*920, size.height)
                     lineTo(0f, size.height)
                 }
-            }
-        )
-        Slider(
-            value = sliderPosition2,
-            onValueChange = {
-                sliderPosition2 = it
             }
         )
     }
